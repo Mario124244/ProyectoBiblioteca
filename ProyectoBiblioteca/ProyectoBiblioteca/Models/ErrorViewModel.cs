208520c772233c7c01036b269193a109bc8bad17
Mario124244 <jesusboli8@.com>
2024-05-08 12:04:12 -0600

    Inicio del proyecto

---
+++ ProyectoBiblioteca/ProyectoBiblioteca/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoBiblioteca.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
