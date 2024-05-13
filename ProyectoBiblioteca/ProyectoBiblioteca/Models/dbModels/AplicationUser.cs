dd34bcdff0a836f1a9fc8ad3377ae9a264498f65
Mario124244 <jesusboli8@.com>
2024-05-13 00:32:20 -0600

    Restaure version anterior debido a que la base de datos me marca un error de conversion de string a int

---
+++ ProyectoBiblioteca/ProyectoBiblioteca/Models/dbModels/AplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace ProyectoBiblioteca.Models.dbModels
+{
+    public class AplicationUser : IdentityUser<int>
+    {
+        
+
+        
+
+        [InverseProperty("Usuario")]
+        public virtual ICollection<Aviso> Avisos { get; set; } = new List<Aviso>();
+
+        [InverseProperty("Usuario")]
+        public virtual ICollection<ReservacionCubiculo> ReservacionCubiculos { get; set; } = new List<ReservacionCubiculo>();
+
+        [InverseProperty("Usuario")]
+        public virtual ICollection<ReservacionMesa> ReservacionMesas { get; set; } = new List<ReservacionMesa>();
+
+        [InverseProperty("Usuario")]
+        public virtual ICollection<Reseña> Reseñas { get; set; } = new List<Reseña>();
+
+        
+    }
+}
