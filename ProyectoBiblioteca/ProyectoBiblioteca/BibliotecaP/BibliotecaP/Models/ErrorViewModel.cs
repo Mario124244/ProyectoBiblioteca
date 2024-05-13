561d9e24b387b46c676c46c15c1e27e7e3780e42
Mario124244 <jesusboli8@.com>
2024-05-13 00:54:11 -0600

    Empezare de 0 el proyecto

---
+++ ProyectoBiblioteca/ProyectoBiblioteca/BibliotecaP/BibliotecaP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BibliotecaP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- ProyectoBiblioteca/ProyectoBiblioteca/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProyectoBiblioteca.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
